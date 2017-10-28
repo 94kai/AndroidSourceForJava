00afa41cf58e2eb4ad4f7a9367196af244e6f645
薛凯 <3440395@qq.com>
2017-10-28 18:58:48 +0800

    第一次提交

---
+++ api25_7.1/src/main/java/xk/MainActivity.java
@@ -0,0 +1,22 @@
+package xk;
+
+import android.annotation.Nullable;
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by xuekai on 2017/8/9.
+ */
+
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+//        View.MeasureSpec.getMode();
+//        View.MeasureSpec.getSize();
+//        Mode                              size     result
+//        View.MeasureSpec.AT_MOST;        100dp    最多是100dp，也可以少
+//        View.MeasureSpec.EXACTLY;        100dp    就是100dp
+//        View.MeasureSpec.UNSPECIFIED;    不确定
+    }
+}
